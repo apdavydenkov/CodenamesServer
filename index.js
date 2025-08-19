d377a6c17c0f080b59bd65dd85e65a89905ffdd1
Alexey WDDT <85016715+apdavydenkov@users.noreply.github.com.com>
2025-08-19 19:45:50 +0300

    Add CORS middleware for API endpoints

--- index.js
+++ index.js
@@ -21,6 +21,19 @@ const gameStats = GameStatsFactory.create();
 const aiService = new AIServerService();
 const aiGamesFile = new AIGamesFileService();
 
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
